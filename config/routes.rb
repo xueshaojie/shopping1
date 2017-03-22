acd31c34938098fdba3a60d052df3c8d12e9a03d
xueshaojie <xueshaojie8@gmail.com>
2017-03-22 17:32:37 +0800

    add admin console

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,13 @@
 Rails.application.routes.draw do
-  get 'sessions/new'
-
-  get 'users/new'
-
   root 'welcome#index'
 
   resources :users
   resources :sessions
-  delete '/logout' => 'session#destroy', as: :logout 
+  delete '/logout' => 'session#destroy', as: :logout
+
+  namespace :admin do
+    root 'sessions#new'
+    resources :sessions
+    resources :categories 
+  end
 end
